f57f07c560ae018b82ec091b73b0b9cc41c8062d
Zsolt Füzesi <zsolt.fuzesi@idomsoft.hu>
2020-01-13 13:40:37 +0100

    setup new project with unit test framework

---
+++ src/test/java/dojoapp2/AppTest.java
@@ -0,0 +1,13 @@
+package dojoapp2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
